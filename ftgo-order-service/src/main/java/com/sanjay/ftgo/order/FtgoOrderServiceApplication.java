1d24ce0286a72b59fabd34d18dd434dadbfb0928
Sanjay Pradhan <sanjaykpradhan10@gmail.com>
2026-06-25 19:41:50 -0700

    feat: add ftgo-order-service stub with hexagonal package structure

---
+++ ftgo-order-service/src/main/java/com/sanjay/ftgo/order/FtgoOrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtgoOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtgoOrderServiceApplication.class, args);
+    }
+}
