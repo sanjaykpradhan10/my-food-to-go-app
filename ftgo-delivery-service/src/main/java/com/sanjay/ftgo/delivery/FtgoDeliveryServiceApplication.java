ef22af70ebe618e4375e9aa71711029853c25491
Sanjay Pradhan <sanjaykpradhan10@gmail.com>
2026-06-25 19:41:50 -0700

    feat: add ftgo-delivery-service stub with hexagonal package structure

---
+++ ftgo-delivery-service/src/main/java/com/sanjay/ftgo/delivery/FtgoDeliveryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtgoDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtgoDeliveryServiceApplication.class, args);
+    }
+}
---
+++ ftgo-delivery-service/src/test/java/com/sanjay/ftgo/delivery/FtgoDeliveryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtgoDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
