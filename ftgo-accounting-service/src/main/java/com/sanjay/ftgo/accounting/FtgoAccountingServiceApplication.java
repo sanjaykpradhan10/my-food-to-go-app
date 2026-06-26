d21bef14941770f65e8b6666f02bc2192788a34a
Sanjay Pradhan <sanjaykpradhan10@gmail.com>
2026-06-25 19:41:50 -0700

    feat: add ftgo-accounting-service stub with hexagonal package structure

---
+++ ftgo-accounting-service/src/main/java/com/sanjay/ftgo/accounting/FtgoAccountingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtgoAccountingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtgoAccountingServiceApplication.class, args);
+    }
+}
---
+++ ftgo-accounting-service/src/test/java/com/sanjay/ftgo/accounting/FtgoAccountingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtgoAccountingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
