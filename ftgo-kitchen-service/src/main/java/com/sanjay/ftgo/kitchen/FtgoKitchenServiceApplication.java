186b2502f57cd070b9fb43f961953b5b35dc5df3
Sanjay Pradhan <sanjaykpradhan10@gmail.com>
2026-06-25 19:41:50 -0700

    feat: add ftgo-kitchen-service stub with hexagonal package structure

---
+++ ftgo-kitchen-service/src/main/java/com/sanjay/ftgo/kitchen/FtgoKitchenServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtgoKitchenServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtgoKitchenServiceApplication.class, args);
+    }
+}
---
+++ ftgo-kitchen-service/src/test/java/com/sanjay/ftgo/kitchen/FtgoKitchenServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sanjay.ftgo.kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtgoKitchenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
