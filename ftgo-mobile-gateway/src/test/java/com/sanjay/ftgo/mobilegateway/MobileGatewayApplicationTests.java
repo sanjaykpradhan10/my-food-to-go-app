f290359e53f91a33e1ec480b601dd80af92da875
Sanjay Pradhan <sanjaykpradhan10@gmail.com>
2026-07-29 20:24:58 -0700

    feat: add ftgo-mobile-gateway BFF routing passthroughs (create/cancel/revise order)

---
+++ ftgo-mobile-gateway/src/test/java/com/sanjay/ftgo/mobilegateway/MobileGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sanjay.ftgo.mobilegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class MobileGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
